4a5871166029015462a92aff1200bb3085096118
nicholaswmin <nik.kyriakides@gmail.com>
2020-06-17 15:59:58 +0300

    remove body parser

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 
 const cookieParser = require('cookie-parser')
 const csrf = require('csurf')
-const bodyParser = require('body-parser')
 const express = require('express')
 const ejs = require('ejs')
 
 // setup route middlewares
 const csrfProtection = csrf({ cookie: true })
-const parseForm = bodyParser.urlencoded({ extended: false })
 
 // create express app
 const app = express()
